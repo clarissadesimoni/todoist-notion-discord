c014bdfb64fd7ccb4e4ac792ad89a00ac786268d
Clarissa De Simoni <clarissa.de.simoni@gmail.com>
2021-06-17 17:46:49 +0200

    notion utility functions (start)

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const discord = new require('discord.js').Client.constructor();
 var crypto = require('crypto');
+const notion = new require('@notionhq/client').constructor({auth: process.env.NOTION_API_KEY});
+
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
+
+const notion = new Client({auth: process.env.NOTION_API_KEY});
 
 app = express();
 app.use(express.urlencoded({ extended: true }));
@@ -15,8 +18,12 @@ process.on('unhandledRejection', error => {
     console.log('unhandledRejection', error.message);
 });
 
-client.on('ready', () => {
-    client.users.fetch(process.env.MY_USER_ID).then(user => user.send('Hey, The bot is up!'));
+function message_user(text) {
+    discord.users.fetch(process.env.MY_USER_ID).then(user => user.send(text));
+}
+
+discord.on('ready', () => {
+    message_user('Hey, The bot is up!');
 });
 
 app.get('', (req, res) => {
@@ -29,33 +36,36 @@ app.post('', (req, res) => {
         var computed_hmac = crypto.createHmac('sha256', process.env.TODOIST_CLIENT_SECRET).update(JSON.stringify(req.body)).digest('base64');
         if(delivered_hmac === computed_hmac) {
             if(req.body.event_name === 'item:added' && req.body.event_data.description === '') {
+                message_user('A new task has been added to Todoist');
                 // this task is in todoist but not on notion
                 
                 // add task to notion
                 // idea: ask user for data
 
-
+                // remeber discord's user.dmchannel.awaitmessages
             } else {
                 if(req.body.event_name === 'item:completed' && req.body.event_data.description !== '') {
                     // this task is completed on todoist but not on notion
                     // complete task on notion
+                    message_user('You can update your tasklist if you want');
                 }else {
                     if(req.body.event_name === 'item:updated' && req.body.event_data.description !== '') {
+                        message_user('A task has been updated Todoist');
                         // this task has to be updated in notion
                         // update task in notion
                     }
                 }
             }
-            // client.users.fetch(process.env.MY_USER_ID).then(user => user.send('You can update your tasklist if you want'));
+            // message_user('You can update your tasklist if you want');
             res.status(200).send('Event handled');
         } else {
-            client.users.fetch(process.env.MY_USER_ID).then(user => user.send('A 403 (Unauthorized) status code has been sent to a request'));
+            message_user('A 403 (Unauthorized) status code has been sent to a request');
             res.status(403).send('Unauthorized');
             console.log(`delivered_hmac: ${delivered_hmac}\ncomputed_hmac: ${computed_hmac}\n`)
             console.log(req.body);
         }
     } else {
-        client.users.fetch(process.env.MY_USER_ID).then(user => user.send('A 400 (Bad request) status code has been sent to a request'));
+        message_user('A 400 (Bad request) status code has been sent to a request');
         res.status(400).send('Bad request');
     }
     //handle notion
@@ -64,7 +74,7 @@ app.post('', (req, res) => {
 })
 
 
-client.login(process.env.BOT_TOKEN);
+discord.login(process.env.BOT_TOKEN);
 app.listen(PORT, () => {
 	console.log(`App up at port ${PORT}`);
 });
---
+++ notion_utility.js
@@ -0,0 +1,49 @@
+var notionHelper = (function () {
+	var my = {};
+
+    require('dotenv').config();
+
+    my.api = new Client({auth: process.env.NOTION_API_KEY});
+    my.prova = function () {
+        return 'it works!';
+    }
+
+	my.simplifyPage = function (
+        page,
+        attrsToRemove = ['object', 'created_time', 'last_edited_time', 'archived'],
+        propsToRemove = ["isNextMonth?", "Progress bar", "previousDoneFormula", "Done", "childrenDone", "Due date", "isThisWeek?", "isProjectActive?", "Completed?", "previousDone", "previousToScheduleCount", "Previous", "Children tasks", "toSchedule", "parentPreviousDone", "availableNow", "Parent task", "isNextWeek?", "progressInt", "isThisMonth?", "Next"]
+    ) {
+        for(const field of attrsToRemove) {
+            delete page[field];
+        }
+        for(const field of propsToRemove) {
+            delete page.properties[field];
+        }
+        page.parent = page.parent.database_id;
+        for(const field in page.properties) {
+            if(page.properties[field].type === 'formula') {
+            page.properties[field] = page.properties[field].formula[page.properties[field].formula.type];
+            } else if(page.properties[field].type === 'rollup') {
+            page.properties[field] = page.properties[field].rollup[page.properties[field].rollup.type];
+            } else if(page.properties[field].type === 'relation') {
+            page.properties[field] = page.properties[field].relation.map(rel => rel.id);
+            } else if(page.properties[field].type === 'date') {
+            page.properties[field] = page.properties[field].date;
+            } else if(page.properties[field].type === 'checkbox') {
+            page.properties[field] = page.properties[field].checkbox;
+            } else if(page.properties[field].type === 'number') {
+            page.properties[field] = page.properties[field].number;
+            } else if(page.properties[field].type === 'title') {
+            page.properties[field] = page.properties[field].title[0].plain_text;
+            }else if(page.properties[field].type === 'rich_text') {
+            page.properties[field] = page.properties[field].rich_text.map(txt => txt.plain_text)
+            }
+        }
+        return page;
+    };
+
+	return my;
+}());
+
+// mymodule.js
+module.exports = notionHelper;
