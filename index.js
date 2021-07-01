00375381ccb41f0a28fac8a94cb350469277d052
Clarissa De Simoni <clarissa.de.simoni@gmail.com>
2021-07-01 15:50:54 +0200

    bug fixes

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var app = express().use(express.urlencoded({ extended: true })).use(express.json
 
 process.on('unhandledRejection', error => {
     // Will print "unhandledRejection err is not defined"
-    console.log('unhandledRejection', error.reason.message);
+    console.log('unhandledRejection', error.reason);
 });
 
 function message_channel(text) {
