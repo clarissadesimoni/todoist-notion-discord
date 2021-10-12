757142f5862632bfc16e8e56b18f2dbb19619e42
Clarissa De Simoni <clarissa.de.simoni@gmail.com>
2021-10-12 02:23:53 +0200

    testing

--- notion_utility.js
+++ notion_utility.js
@@ -70,7 +70,6 @@ var notionHelper = (function () {
     }
 
     my.updateTask = async function(notion_page_id, name, todoist_project_id, do_date, priority, in_discord) {
-        console.log(my.getProject("todoist_id", todoist_project_id).notion_id || 'error');
         var req_body = {
             page_id: notion_page_id,
             properties: {
